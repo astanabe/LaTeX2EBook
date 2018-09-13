b35489049c549e8645621cf83f61cc6e860773d6
Akifumi S. Tanabe <akifumi.tanabe+github@gmail.com>
2018-09-14 07:22:54 +0900

    Update convertbll.pl

--- convertbbl.pl
+++ convertbbl.pl
@@ -3,7 +3,7 @@ use open ':encoding(utf8)';
 use open ':std';
 while (<>) {
 	while (/(\\href\{[^\{\}]+\}\{[^\{\}]+)\_([^\{\}]+\})/) {
-		s/(\\href\{[^\{\}]+\}\{[^\{\}]+)\_([^\{\}]+\})/$1\\textunderscore$2/;
+		s/(\\href\{[^\{\}]+\}\{[^\{\}]+)\_([^\{\}]+\})/$1\{\\textunderscore\}$2/;
 	}
 	print;
 }
