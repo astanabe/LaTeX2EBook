8143ae19c8d62a554c054a201f8df25fc4e57a78
Akifumi S. Tanabe <akifumi.tanabe+github@gmail.com>
2018-09-15 03:52:20 +0900

    Add image conversion

--- convertbbl.pl
+++ convertbbl.pl
@@ -2,8 +2,20 @@ use utf8;
 use open ':encoding(utf8)';
 use open ':std';
 while (<>) {
-	while (/(\\href\{[^\{\}]+\}\{[^\{\}]+)\_([^\{\}]+\})/) {
-		s/(\\href\{[^\{\}]+\}\{[^\{\}]+)\_([^\{\}]+\})/$1\{\\textunderscore\}$2/;
+	while (/(\\href\{[^\{\}]+\}\{.+?)([\_\-\.\/])([^\{\}]+\})/) {
+		my $char = $2;
+		if ($char eq '_') {
+			s/(\\href\{[^\{\}]+\}\{.+?)\_([^\{\}]+\})/$1\{\\textunderscore\}\\allowbreak\{\}$2/;
+		}
+		elsif ($char eq '-') {
+			s/(\\href\{[^\{\}]+\}\{.+?)\-([^\{\}]+\})/$1\{-\}\\allowbreak\{\}$2/;
+		}
+		elsif ($char eq '.') {
+			s/(\\href\{[^\{\}]+\}\{.+?)\.([^\{\}]+\})/$1\{.\}\\allowbreak\{\}$2/;
+		}
+		elsif ($char eq '/') {
+			s/(\\href\{[^\{\}]+\}\{.+?)\/([^\{\}]+\})/$1\{\/\}\\allowbreak\{\}$2/;
+		}
 	}
 	print;
 }
